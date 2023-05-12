4b09ed96eee9bf6bd484cbf275efd593df4655ee
tumzied_ <50454992+tz01x@users.noreply.github.com>
2023-05-12 18:38:28 +0000

    added video vocal and transcription gen

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-from yt_dlp import YoutubeDL
-from settings import BASE_DIR
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+from datetime import datetime
+from pathlib import Path
 
-def download_video(video_url:str):
+import whisper_timestamped
+from yt_dlp import YoutubeDL
+
+from settings import BASE_DIR
+from utils import write_srt
+
+MODEL_TYPES = ["tiny", "base", "small", "medium", "large"]
+
+
+def download_video(video_url: str):
     ydl_opts = {
-            "format": "mp4",
-            "outtmpl": str(BASE_DIR / "mp4/%(title)s.%(ext)s"),
-            "quiet": True,
+        "format": "mp4",
+        "outtmpl": str(BASE_DIR / "mp4/%(title)s.%(ext)s"),
+        "quiet": True,
     }
 
     if video_url == None or video_url.strip() == "":
@@ -16,6 +30,53 @@ def download_video(video_url:str):
         download_file_path = ydl.prepare_filename(info)
     return download_file_path
 
-print(
-    download_video('https://www.youtube.com/watch?v=668nUCeBHyY&pp=ygUQc21hbGwgdmlkZW8gY2xpcA%3D%3D')
-)
+
+def extract_vocal(media: str):
+
+    # media_extension = Path(media).suffix
+    # media_new_filepath = tempfile.gettempdir() / Path(
+    #     "tempfreesubtitle/main"
+    # ).with_suffix(media_extension)
+
+    # Path(media_new_filepath).parent.mkdir(parents=True, exist_ok=True)
+    # shutil.copy(media, media_new_filepath)
+
+    # demucs_directory = tempfile.gettempdir()
+
+    # filename = Path(media).name
+
+    # filename,*_=filename.split(".")
+
+    media_new_filepath = media
+    demucs_directory = str(BASE_DIR/'vocals')
+
+    # sys.path.append(str(BASE_DIR/'ffmpeg/'))
+
+    # "demucs --two-stems=vocals mp4/1min.mp4 -o tmp/ --filename {track}/{stem}.{ext}"" # FileName/VOCAL.wav
+    cmd = rf'demucs --two-stems=vocals "{media_new_filepath}" -o "{demucs_directory}" --filename "{{stem}}.{{ext}}"'
+
+    subprocess.run(cmd, check=True)
+
+
+def create_srt():
+    device = 'cpu'  # 'gpu'
+    model_type = MODEL_TYPES[0]
+
+    used_model = whisper_timestamped.load_model(model_type, device=device)
+    result = whisper_timestamped.transcribe(
+        model=used_model,
+        audio=str(BASE_DIR/'vocals/htdemucs/vocals.wav'),
+        vad=True,
+        verbose=False,
+    )
+    write_srt(result["segments"], 'abcd.srt')
+
+
+# media_filepath = download_video('https://www.youtube.com/watch?v=xmx07H3sn1w')
+
+
+# $env:Path = "$pwd\ffmpeg;$env:Path"
+# extract_vocal(
+#     str(BASE_DIR / 'mp4/4 Easy Ways To Make Small Talk With People.mp4'))
+
+create_srt()
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+from typing import Iterator, TextIO
+
+
+
+def format_timestamp(seconds: float, always_include_hours: bool = False):
+    assert seconds >= 0, "non-negative timestamp expected"
+    milliseconds = round(seconds * 1000.0)
+
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
+
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    hours_marker = f"{hours}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+
+
+def write_srt(transcript: Iterator[dict], file: TextIO):
+    """
+    Write a transcript to a file in SRT format.
+    Example usage:
+        from pathlib import Path
+        from whisper.utils import write_srt
+        result = transcribe(model, audio_path, temperature=temperature, **args)
+        # save SRT
+        audio_basename = Path(audio_path).stem
+        with open(Path(output_dir) / (audio_basename + ".srt"), "w", encoding="utf-8") as srt:
+            write_srt(result["segments"], file=srt)
+    """
+    with open(file, "w", encoding="UTF-8") as f:
+        for segment in transcript:
+            # write srt lines
+            id = segment["id"]
+            start = format_timestamp(segment["start"], always_include_hours=True)
+            end = format_timestamp(segment["end"], always_include_hours=True)
+            text = segment["text"].strip().replace("-->", "->")
+
+            f.write(f"{id}\n{start} --> {end}\n{text}\n\n")
